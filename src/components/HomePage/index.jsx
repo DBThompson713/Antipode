cfd11eb40682402b5c65af65df896a32d5d333cb
DB Thompson <dbthompson.au@gmail.com>
2020-05-18 15:59:20 +1000

    results page start

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,36 +1,22 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-// import { increment, decrement, isLogged } from './actions';
-
-import { useHistory } from 'react-router-dom';
+import ResultsPage from './../ResultsPage';
 import SearchForm from './../SearchForm';
 import './styles.scss';
 
 const Homepage = () => {
   const [location, setLocation] = useState('');
-  const [antipodeData, setAntipodeData] = useState({
-    location: '',
-    antipodeLocation: '',
-    fullName: '',
-    lat: '',
-    long: '',
-    oppLat: '',
-  });
-  const [antipodeLocation, setAntipodeLocation] = useState('');
-  const [fullName, setFullName] = useState('');
-  const [geoLat, setGeoLat] = useState();
-  const [oppLong, setOppLong] = useState({});
-  const [oppLat, setOppLat] = useState({});
-  const [geoLong, setGeoLong] = useState({});
+  const [oppLong, setOppLong] = useState();
+  const [oppLat, setOppLat] = useState();
+  const [resultsData, setResultsData] = useState({});
+  const [antipodeData, setAntipodeData] = useState({});
+  const [hideResults, setHideResults] = useState(false);
+
   const inputElement = React.createRef();
   const apiKey = process.env.REACT_APP_COORDS_API;
   const coordsCall = `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${apiKey}`;
   const antipodeCall = `https://api.opencagedata.com/geocode/v1/json?q=${oppLat}+${oppLong}&key=${apiKey}`;
-  //
 
-  //
   const handleInput = (e) => {
     const newLocation = e.target.value;
     setLocation(newLocation);
@@ -48,50 +34,31 @@ const Homepage = () => {
   const GetCoords = async (location) => {
     await axios
       .get(coordsCall)
-      .then((results) => {
-        // console.log(results);
-
-        const geo = results.data.results[0].geometry;
-
-        setGeoLat(geo.lat);
-        setFullName(results.data.results[0].formatted);
-        setGeoLong(geo.lng);
-        setOppLat(geo.lat * -1);
-        setOppLong(geo.lng * -1);
-
-        console.log(antipodeCall);
-        axios.get(antipodeCall).then(
-          console.log(results)
-          // setAntipodeLocation()
-        );
-
-        console.log(
-          `${fullName}:LAT:${geoLat}:LONG${geoLong}::::::::${antipodeLocation}LAT:${oppLat}:LONG:${oppLong}`
-        );
-
-        setAntipodeData({
-          location: { location },
-          antipodeLocation: { antipodeLocation },
-          fullName: { fullName },
-          lat: { geoLat },
-          long: { geoLong },
-          oppLat: { oppLat },
-        });
-        console.log(antipodeData);
+      .then((res) => {
+        setOppLong(res.data.results[0].geometry.lng * -1);
+        setOppLat(res.data.results[0].geometry.lat * -1);
+        setResultsData(res.data.results[0]);
       })
-      .catch(console.log(''));
+      .then(
+        await axios.get(antipodeCall).then((results) => {
+          setAntipodeData(results.data.results[0]);
+        })
+      )
+      .catch('Something went wrong');
+    setHideResults(!hideResults);
   };
 
   return (
     <div className='home-container'>
-      <div className='search'>
-        <SearchForm
-          handleInput={handleInput}
-          location={location}
-          inputElement={inputElement}
-          getAntipode={getAntipode}
-        />
-        <Link to='/ResultsPage' data={antipodeData}>
+      {!hideResults && (
+        <div className='search'>
+          <SearchForm
+            handleInput={handleInput}
+            location={location}
+            inputElement={inputElement}
+            getAntipode={getAntipode}
+          />
+
           <div
             role='button'
             className='search-button'
@@ -100,8 +67,17 @@ const Homepage = () => {
           >
             Search
           </div>
-        </Link>
-      </div>
+        </div>
+      )}
+
+      {hideResults && (
+        <ResultsPage
+          data={resultsData}
+          antipode={antipodeData}
+          oppLat={oppLat}
+          oppLong={oppLong}
+        />
+      )}
     </div>
   );
 };
--- src/components/ResultsPage/index.jsx
+++ src/components/ResultsPage/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './styles.scss';
 
-const ResultsPage = () => {
+const ResultsPage = (props, antipode) => {
+  const data = props.data;
+  const antiData = props.antipode;
+
+  //   YOUR LOCATION
+  const locationName = data.formatted;
+  const latitude = props.data.geometry.lat;
+  const longitude = props.data.geometry.lng;
+
+  //   ANTIPODE
+  const antipodeName = antiData.formatted;
+  const oppLatitude = props.oppLat;
+  const oppLongitude = props.oppLong;
+
   return (
     <div className='results-container'>
-      <h1>RESULTS</h1>
+      <section className='results your-location '>
+        <h1>{locationName}</h1>
+        <h2>Lat:{latitude}</h2>
+        <h2>Long: {longitude}</h2>
+      </section>
+      <section className='results antipode-location'>
+        <h1>{antipodeName}</h1>
+        <h2>Lat:{oppLatitude}</h2>
+        <h2>Long: {oppLongitude}</h2>
+      </section>
     </div>
   );
 };
