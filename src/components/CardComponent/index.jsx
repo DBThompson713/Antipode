c84accc61af7ab18d794de6bc2db90f93d643a30
DB Thompson <dbthompson.au@gmail.com>
2020-05-27 19:34:09 +1000

    limited the the location title

--- src/components/CardComponent/index.jsx
+++ src/components/CardComponent/index.jsx
@@ -19,7 +19,7 @@ const Card = (props) => {
       >
         <div>
           <h1 className='title-coords'>
-            {props.locationName.substring(0, 44)}{' '}
+            {props.locationName.substring(0, 17)}{' '}
           </h1>
           <p>
             <span className='coords'>
